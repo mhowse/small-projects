a05dc7acc1a3ab0e901e7cc52f3fb016be6148ce
mhowse <mikhaila.howse@gmail.com>
2016-02-08 17:30:03 +1300

    Start of new project, a simple english-german translator.

---
+++ SmallProjectsGit/src/smallprojectsgit/simpleTranslator.java
@@ -0,0 +1,17 @@
+/**simpleTranslator.java
+ * mhowse February  2016.
+ * 
+ */
+
+/**
+ * This will be a simple translation program, 
+ * translating SIMPLE sentances from english to german and vice versa. 
+ * it will translate literally, not meaningfully. 
+ * A later translation program  will start to use grammar in its translating. 
+*/
+
+public class simpleTranslator {
+    
+    
+    
+}//end class

5fcdf5230da7b920499fe58f62848d41c4dfbde9
mhowse <mikhaila.howse@gmail.com>
2016-02-10 13:21:24 +1300

    Setting direction of the translation. german to english or vice versa

--- SmallProjectsGit/src/smallprojectsgit/simpleTranslator.java
+++ SmallProjectsGit/src/smallprojectsgit/simpleTranslator.java
@@ -3,6 +3,7 @@
  * 
  */
 
+import java.util.*;
 /**
  * This will be a simple translation program, 
  * translating SIMPLE sentances from english to german and vice versa. 
@@ -11,7 +12,34 @@
 */
 
 public class simpleTranslator {
+    private static String direction = ""; 
     
+    /**main method
+     * 
+     * @param args 
+     */
+    public static void main (String [] args){
+        
+    }
     
+    /**
+     * simple translate constructor 
+     * the string indicates which direction the translation will run 
+     */
+    private simpleTranslator (String i ){
+        
+    }
+    
+    /** chooses which way the translation will 
+     * run in, English to German v.s. German to English
+     */
+    private static void chooseTranslation () {
+     Scanner scan = new Scanner (System.in); 
+     System.out.println("What language do you want to translate to?");
+       while (scan.hasNextLine()){
+            direction = scan.nextLine();
+        }
+    
+}
     
 }//end class

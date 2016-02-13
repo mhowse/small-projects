9d5ab68e3a4ecaa70d1e60a54bf2f65d71681572
mhowse <mikhaila.howse@gmail.com>
2016-02-13 17:39:42 +1300

    Beginning of the translation setup. Starting to fill in the spreadsheet, Using the jOpenDocument libraries to accesss the spreadsheet.

---
+++ SmallProjectsGit/src/smallprojectsgit/translator/simpleTranslator.java
@@ -0,0 +1,126 @@
+/**simpleTranslator.java
+  * mhowse February  2016.
+  * 
+  */
+
+
+package smallprojectsgit.translator; 
+
+import java.util.*;
+import java.io.*; 
+import org.jopendocument.dom.spreadsheet.MutableCell;
+import org.jopendocument.dom.spreadsheet.Sheet;
+import org.jopendocument.dom.spreadsheet.SpreadSheet;
+/** The translations will be done word for word, from  a stored dictionary  spreadsheet. 
+  * This stored dictionary will be based on the top 1000 used German words, 
+  * as found at 
+  * http://www.languagedaily.com/learn-german/vocabulary/common-german-words
+  * The spreadsheet manipulation comes from the jOpenDocument libraries.
+  */
+
+
+public class simpleTranslator {
+  private static String direction = ""; 
+  private String wordToTranslate ="";
+  private String currentCell = ""; 
+  private int rowCount;
+  private int colCount;
+  
+   public void readODS(File file)
+      {
+        Sheet sheet;
+        try {
+             //Getting the 0th sheet for manipulation| pass sheet name as string
+             sheet = SpreadSheet.createFromFile(file).getSheet(0);
+              
+             //Get row count and column count
+             int nColCount = sheet.getColumnCount();
+             int nRowCount = sheet.getRowCount();
+             rowCount =nRowCount; 
+             colCount=nColCount;
+
+             System.out.println("Rows :"+nRowCount);
+             System.out.println("Cols :"+nColCount);
+             //Iterating through each row of the selected sheet
+             MutableCell cell = null;
+             for(int nRowIndex = 0; nRowIndex < nRowCount; nRowIndex++)
+             {
+               //Iterating through each column
+               int nColIndex = 0;
+               for( ;nColIndex < nColCount; nColIndex++)
+               {
+                 cell = sheet.getCellAt(nColIndex, nRowIndex);
+                 System.out.print(cell.getValue()+ " ");
+                }
+                System.out.println();
+              }
+
+            } catch (IOException e) {
+              e.printStackTrace();
+            }
+      }
+      
+  
+  /**main method
+    * 
+    * @param args 
+    */
+  public static void main (String [] args) throws Exception{
+    chooseTranslation(); //set direction
+    //quick fix in case user derps, only one direction atm. 
+    direction = "German"; 
+    simpleTranslator simple = new simpleTranslator(direction); //create translator
+    //do i want a seperate translator for each direction? and just switch which one is being used?
+  
+  }
+  
+  
+  /**
+   * simple translate constructor 
+   * the string indicates which direction the translation will run 
+   */
+  private simpleTranslator (String i ){
+    if (direction =="German"){
+      // Load the specific dictionary file.
+      try {
+ File file = new File("dictionary.ods");
+    System.out.println("loading file");
+ //make a sheet to work with
+
+ final Sheet sheet = SpreadSheet.createFromFile(file).getSheet(0);
+ } catch (Exception e){
+   System.out.println("exception at sheet creation");
+ }
+    }
+ //setup complete, start taking and translating words. 
+   Scanner scan = new Scanner(System.in);
+   System.out.println("Type in the german word, and an english translation will appear"); 
+   wordToTranslate = scan.nextLine(); 
+   System.out.println(colCount +"cols accross");
+   System.out.println(rowCount+"rows down");
+   /*
+   sheet.getCellAt("I10").setValue(new Date());
+ // Change strings.
+ sheet.setValueAt("Filling test", 1, 1);
+ sheet.getCellAt("B27").setValue("On site support");
+ // Change number.
+ sheet.getCellAt("F24").setValue(3);
+ // Or better yet use a named range
+ // (relative to the first cell of the range, wherever it might be).
+ sheet.getSpreadSheet().getTableModel("Products").setValueAt(1, 5, 4);
+ */
+
+    }
+  
+  
+  /** chooses which way the translation will 
+    * run in, English to German v.s. German to English
+    */
+  private static void chooseTranslation () {
+    Scanner scan = new Scanner (System.in); 
+    System.out.println("What language do you want to translate to?"); 
+    direction = scan.nextLine();
+    scan.close();
+  }
+  
+}//end class

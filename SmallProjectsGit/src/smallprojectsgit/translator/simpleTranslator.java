530a8f6957e470b018a1c0ae5cdf56be7982ccde
mhowse <mikhaila.howse@gmail.com>
2016-02-15 11:27:00 +1300

    all fixed, the sheet now loads. Added a getPathDirectory to handle the  path setting for loading the dictionary.ods. This includes the formatting of the path  string -especially the changing of slash directions (forwards vs. back slashes). This also allows for forwards planning for later options of having different dictionary sheets for different forms of translations.

--- SmallProjectsGit/src/smallprojectsgit/translator/simpleTranslator.java
+++ SmallProjectsGit/src/smallprojectsgit/translator/simpleTranslator.java
@@ -1,97 +1,120 @@
-        /**simpleTranslator.java
-          * mhowse February  2016.
-          * 
-          */
+/**simpleTranslator.java
+  * mhowse February  2016.
+  * 
+  */
 
 
-        package smallprojectsgit.translator; 
+package smallprojectsgit.translator; 
 
-        import java.util.*;
-        import java.io.*; 
-        //import org.jopendocument.dom.spreadsheet.MutableCell;
-        import org.jopendocument.dom.spreadsheet.Sheet;
-        import org.jopendocument.dom.spreadsheet.SpreadSheet;
-        /** The translations will be done word for word, from  a stored dictionary  spreadsheet. 
-          * This stored dictionary will be based on the top 1000 used German words, 
-          * as found at 
-          * http://www.languagedaily.com/learn-german/vocabulary/common-german-words
-          * The spreadsheet manipulation comes from the jOpenDocument libraries.
-          */
-
-
-        public class simpleTranslator {
-          private static String direction = ""; 
-          private String wordToTranslate ="";
-          private String currentCell = ""; 
-          private int rowCount;
-          private int colCount;
-          private File file; 
+import java.util.*;
+import java.io.*; 
+import java.nio.*;
+import java.net.*;
+import java.lang.Class.*;
+//import org.jopendocument.dom.spreadsheet.MutableCell;
+import org.jopendocument.dom.spreadsheet.Sheet;
+import org.jopendocument.dom.spreadsheet.SpreadSheet;
+/** The translations will be done word for word, from  a stored dictionary  spreadsheet. 
+  * This stored dictionary will be based on the top 1000 used German words, 
+  * as found at 
+  * http://www.languagedaily.com/learn-german/vocabulary/common-german-words
+  * The spreadsheet manipulation comes from the jOpenDocument libraries.
+  */
 
 
-          /**main method
-            * 
-            * @param args 
-     * @throws java.lang.Exception 
-            */
-          public static void main (String [] args) throws Exception{
-              System.out.println("running");
-            chooseTranslation(); //set direction
-            //quick fix in case user derps, only one direction atm. 
-            direction = "German"; 
-            simpleTranslator simple = new simpleTranslator(direction); //create translator
-            //do i want a seperate translator for each direction? and just switch which one is being used?
-
-          }
-
-
-          /**
-           * simple translate constructor 
-           * the string indicates which direction the translation will run 
-           */
-          private simpleTranslator (String i ){
-            if (direction.equals("German")){
-              // Load the specific dictionary file.
-              try {
-                  try{
-          file = new File("smallprojectsgit.translator.dictionary.ods");
-            System.out.println("loading file");
-         //issue is that the file can't be found. \
-           } catch (Exception e){
-           System.out.println(e.getMessage());
-           System.out.println("first catch spot");
-         }
-              
-         final Sheet sheet = SpreadSheet.createFromFile(file).getSheet(0);
-         System.out.println("Sheet created");
-          colCount = sheet.getColumnCount();
-          rowCount = sheet.getRowCount();
-          System.out.println("Rows :"+rowCount);
-          System.out.println("Cols :"+colCount);
-                     // so one can iterate through each row of the selected sheet
-                     //this is so one can search for the word. 
-         } catch (Exception e){
-           System.out.println(e.getMessage());
-         }
-            }
-         //setup complete, start taking and translating words. 
-           Scanner scan = new Scanner(System.in);
-           System.out.println("Type in the german word, and an english translation will appear"); 
-           wordToTranslate = scan.nextLine(); 
-           System.out.println(colCount +"cols accross");
-           System.out.println(rowCount+"rows down");
-
-            }
-
+public class simpleTranslator {
+  private static String direction = ""; 
+  private String wordToTranslate ="";
+  private String currentCell = ""; 
+  private int rowCount;
+  private int colCount;
+  private String path =""; // private File file;
+  
+  
+  /**main method
+    * 
+    * @param args 
+    * @throws java.lang.Exception 
+    */
+  public static void main (String [] args) throws Exception{
+    System.out.println("running");
+    chooseTranslation(); //set direction
+    //quick fix in case user derps, only one direction atm. 
+    direction = "German"; 
+    simpleTranslator simple = new simpleTranslator(direction); //create translator
+    //do i want a seperate translator for each direction? and just switch which one is being used?
+    
+  }
+  
+  
+  /**
+   * simple translate constructor 
+   * the string indicates which direction the translation will run 
+   */
+  private simpleTranslator (String i ){
+    if (direction.equals("German")){
+      // Load the specific dictionary file.
+        try{
+          getPathDictionary();
          
-
-          /** chooses which way the translation will 
-            * run in, English to German v.s. German to English
-            */
-          private static void chooseTranslation () {
-              try (Scanner scan = new Scanner (System.in)) {
-                  System.out.println("What language do you want to translate to?");
-                  direction = scan.nextLine();
-              }
-          }
-
-        }//end class
+          File file = new File(path); 
+           
+          //null pointer excpetion here, so either the
+          System.out.println("loading file");
+        final Sheet sheet = SpreadSheet.createFromFile(file).getSheet(0);
+        System.out.println("Sheet created");
+        colCount = sheet.getColumnCount();
+        rowCount = sheet.getRowCount();
+        System.out.println("Rows :"+rowCount);
+        System.out.println("Cols :"+colCount);
+        // so one can iterate through each row of the selected sheet
+        //this is so one can search for the word. 
+      } catch (Exception e){
+        System.out.println("error");
+        System.out.println(e.getMessage());
+        e.printStackTrace(); 
+        System.out.println("error message end");
+      }
+    }
+    //setup complete, start taking and translating words. 
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Type in the german word, and an english translation will appear"); 
+    wordToTranslate = scan.nextLine(); 
+    System.out.println(colCount +"cols accross");
+    System.out.println(rowCount+"rows down");
+    
+  }
+  /**
+   * 
+   */
+  
+  private void getPathDictionary(){
+    String s=""; //adds in the path suffix to load the correct dictionary. 
+    if (direction.equals ("German")){
+    s = "/resources/dictionary.ods";
+    System.out.println("s is set");
+    }
+     String y =System.getProperty("user.dir");
+      System.out.println(s +"\n"+ y);
+     char c='/';
+     y+=s;
+     System.out.println(y);
+     System.out.println("c set");
+     y.replace("\\", "/");
+         path = new String(y);
+         System.out.println(path);
+        
+  }
+  
+  /** chooses which way the translation will 
+    * run in, English to German v.s. German to English
+    */
+  private static void chooseTranslation () {
+    try (Scanner scan = new Scanner (System.in)) {
+      System.out.println("What language do you want to translate to?");
+      direction = scan.nextLine();
+    }
+  }
+  
+  
+}//end class

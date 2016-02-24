2daee8d5eaa339e6f8368f76a2e028daeb809130
mhowse <mikhaila.howse@gmail.com>
2016-02-24 21:58:11 +1300

    more fleshed out

--- SmallProjectsGit/src/smallprojectsgit/asciiSumming.java
+++ SmallProjectsGit/src/smallprojectsgit/asciiSumming.java
@@ -12,16 +12,21 @@ public class asciiSumming{
         System.out.println("Enter a string, this program will summ the ascii values of all characters therein.");
         Scanner scan =new Scanner (System.in);
         String s ="";
-        while (true){
+        while(true){
             int sum =0;
             s=scan.nextLine();
            char[] x= s.toCharArray();
                     for(char c: x){
                         sum +=c;
                     }//end for loop
+        
+        System.out.println("Total is " + sum);
+        
+        if (s.equals("exit")){
+            scan.close();
+            System.exit(0);      
+        }// 
         }//end while loop
-        
-        
-        
+      
     }//end main method
 }//end class

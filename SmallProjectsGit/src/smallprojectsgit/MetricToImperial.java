b23637983a2e185bd88f8d4a58bf3b71615cb09f
mhowse <mikhaila.howse@gmail.com>
2016-04-06 22:34:46 +1200

    continuation of measurement converter, mainly the main method, taking info as file, processing it, and then providing the user with the results of the convert method.

--- SmallProjectsGit/src/smallprojectsgit/MetricToImperial.java
+++ SmallProjectsGit/src/smallprojectsgit/MetricToImperial.java
@@ -6,15 +6,48 @@ package smallprojectsgit;
  */
 
 import java.util.Scanner;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  * Will convert metric input in cm, to feet and inches. 
  */
 public class MetricToImperial {
     
     
-    public static void main(String [] args){
-        Scanner scan = new Scanner(System.in);
+    public  void main(String [] args){
+        List<Integer> metric = new ArrayList<>();
+        List<String> imperial = new ArrayList<>();
+        try {
+            File file = new File("numbers.txt");
+            Scanner scan = new Scanner(file);
+            while (scan.hasNextInt()){
+                metric.add(scan.nextInt()); 
+            }
+        } //end main method.
+        catch (FileNotFoundException ex) {
+            Logger.getLogger(MetricToImperial.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        metric.stream().forEach((s) -> {
+            imperial.add( convert(s));
+        });
+      for(int i =0; i < metric.size(); i++){
+          System.out.println(""+metric.get(i)+"cm equals "+imperial.get(i));
+      }
+
+
         
-    }//end main method. 
+    }// end main method
+     /**
+      * one inch = 2.54 cm. 
+      * @param s
+      * @return 
+      */
+    private String convert(int s){
+        double inch =s/2.54; 
+       return "derp"; 
+    }
     
 }//end class

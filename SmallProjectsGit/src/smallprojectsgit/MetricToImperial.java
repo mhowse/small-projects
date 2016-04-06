4a67f4b128ef556249f768375148289cb35e2bd7
mhowse <mikhaila.howse@gmail.com>
2016-04-06 21:56:22 +1200

    Start of measurement converter

---
+++ SmallProjectsGit/src/smallprojectsgit/MetricToImperial.java
@@ -0,0 +1,20 @@
+package smallprojectsgit;
+
+/**
+ * MetricToImperial.java
+ * mhowse  april 2016
+ */
+
+import java.util.Scanner;
+/**
+ * Will convert metric input in cm, to feet and inches. 
+ */
+public class MetricToImperial {
+    
+    
+    public static void main(String [] args){
+        Scanner scan = new Scanner(System.in);
+        
+    }//end main method. 
+    
+}//end class

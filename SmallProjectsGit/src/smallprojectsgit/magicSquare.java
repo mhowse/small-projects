74b5d5bb4c22bc62c070a7491406bbbc7452ee35
mhowse <mikhaila.howse@gmail.com>
2016-04-19 23:08:28 +1200

    check3 now checks the 2 diagonal sums

--- SmallProjectsGit/src/smallprojectsgit/magicSquare.java
+++ SmallProjectsGit/src/smallprojectsgit/magicSquare.java
@@ -23,11 +23,13 @@ public class magicSquare {
      * such that it is accurate (ie add required lines add to 15).
      */
     private static void check3(int [][] arr){
+        int total;
         for(int i =0; i <3; i++){
             rowCheck(i,arr);
-            //colCheck(i,arr);
-    
+            colCheck(i,arr);
         }
+        diagMain = ((total=( arr[0][0]+arr[0][1]+arr[0][2])) ==goal);
+        diagSecond = ((total=( arr[2][2]+arr[0][1]+arr[2][0])) ==goal);
     }
     /**
      * rowCheck will check the given row, to see if it sums to 15. 
@@ -55,7 +57,32 @@ public class magicSquare {
             }
          } //end method
     
-    
+    /**
+     * colCheck.
+     * Basically a clone of the rowCheck method, the difference being  which cell param i refers to
+     * @param i which column to check.
+     * @param ar 
+     */
+    private static void colCheck(int i, int [][] ar){
+          int total;
+            total = ar[i][0] +ar[i][1]+ar [i][2];
+            if (total == goal){
+                switch (i){
+                    case 0:
+                        colA = true;
+                        break;
+                    case 1:
+                        colB =true;
+                        break;
+                    case 2:
+                        colC = true;
+                        break;
+                    default:
+                        System.out.println("Error in the switch case section of rows for check 3");
+                        System.out.println("i = "+i);            
+                } //end switch;
+            }
+    }
     /**
      * Fills the sample magic square to test. 
      * For now uses the Lo Shu magic square

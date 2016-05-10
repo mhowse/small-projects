1519b0af08fb7c079357d18f6c7ab24e40dcdc67
mhowse <mikhaila.howse@gmail.com>
2016-05-10 23:17:08 +1200

    problem 1, the summing of all multiples of 3 or 5 less then 1000, complete.

--- SmallProjectsGit/src/smallprojectsgit/projectEuler/EulerProblems.java
+++ SmallProjectsGit/src/smallprojectsgit/projectEuler/EulerProblems.java
@@ -8,9 +8,23 @@ package smallprojectsgit.projectEuler;
 public class EulerProblems {
     
     public static void main (String [] args){
-        
+        problem1();
     }//end main method
     
-    
+    /**
+     * problem1 solves the first project Euler problem. 
+     * Find the sum of all the multiples of 3 or 5 below 1000.
+     */
+    private static void problem1(){
+        int sum=0;
+        int count =0;
+        while(count<1000){
+            if(count%3 ==0 ||count %5==0){
+                sum+=count; 
+            }
+            count++;
+        }
+        System.out.println("Sum ="+sum);
+    }
     
 }//end class

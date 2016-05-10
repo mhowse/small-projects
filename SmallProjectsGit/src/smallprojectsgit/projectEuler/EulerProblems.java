09894576fac8168398e9dd44d6efe47fadaf5154
mhowse <mikhaila.howse@gmail.com>
2016-05-10 23:12:21 +1200

    start of the working through the Project Euler ( projecteuler.net ) problems.

---
+++ SmallProjectsGit/src/smallprojectsgit/projectEuler/EulerProblems.java
@@ -0,0 +1,16 @@
+package smallprojectsgit.projectEuler;
+
+/**
+ * EulerProblems.java
+ * mhowse may 2016
+ */
+
+public class EulerProblems {
+    
+    public static void main (String [] args){
+        
+    }//end main method
+    
+    
+    
+}//end class

98031743391cc85b52f304a7a9afd91e19e7a761
mhowse <mikhaila.howse@gmail.com>
2016-05-21 21:40:42 +1200

    Just fixing the isPrime method gets us the result of the 10001 prime is 104759. The 10001 prime is actually 104743. I suspect it is because is prime thinks 0 is a prime and thus counts to the next prime after 104743.

--- SmallProjectsGit/src/smallprojectsgit/projectEuler/EulerProblems.java
+++ SmallProjectsGit/src/smallprojectsgit/projectEuler/EulerProblems.java
@@ -237,6 +237,12 @@ public class EulerProblems {
     
         
     private static boolean isPrime(int num){
+          if (num < 2) return false;
+        if (num == 2) return true;
+        if (num % 2 == 0) return false;
+        for (int i = 3; i * i <= num; i += 2)
+            if (num % i == 0) return false;
         return true;
-    }
+}
+    
 }//end class

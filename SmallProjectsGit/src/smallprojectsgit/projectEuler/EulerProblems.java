2b57d544fbe903b067c6da35dce66161b831b673
mhowse <mikhaila.howse@gmail.com>
2016-05-21 21:52:01 +1200

    Collapsing the isPrime method so that everything happens inside problem7()

--- SmallProjectsGit/src/smallprojectsgit/projectEuler/EulerProblems.java
+++ SmallProjectsGit/src/smallprojectsgit/projectEuler/EulerProblems.java
@@ -218,31 +218,31 @@ public class EulerProblems {
      */
     private static void problem7(){
         int n=1; 
-        int primeCount=1;
+        int primeCount=0;
         boolean found = false;
         while(!found){
             if(n%2 !=0){
-                if(isPrime(n)){
-                primeCount++;    
+                int isPrime=0;
+                 for (int i = 3; i * i <= n; i += 2){
+                     if (n % i == 0) {
+                         isPrime++;
+                     } 
+                 }
+                     if(isPrime==0){
+                         primeCount++;
+                     }
+                  
                 if(primeCount==10001){
                     System.out.println("the 10001 prime is "+n);
                     return;
                 }
             }
-            }
-            n++;
             
+            n++;
         }
-    }
+        }
+    
     
         
-    private static boolean isPrime(int num){
-          if (num < 2) return false;
-        if (num == 2) return true;
-        if (num % 2 == 0) return false;
-        for (int i = 3; i * i <= num; i += 2)
-            if (num % i == 0) return false;
-        return true;
-}
     
 }//end class

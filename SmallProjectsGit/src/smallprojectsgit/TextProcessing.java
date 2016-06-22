ac18d9f342149bba9b81c93f28fe35a2837af246
mhowse <mikhaila.howse@gmail.com>
2016-06-22 22:44:58 +1200

    Start of a file of methods to handle simple, monotonous but time consuming tasks involving text. I.E. converting date formats. First off is stripping excess data from time stamps, to leave just the date, in dd/mm/yyyy form rather then yyyy-mm-dd plus time.

---
+++ SmallProjectsGit/src/smallprojectsgit/TextProcessing.java
@@ -0,0 +1,18 @@
+package smallprojectsgit;
+
+/**
+ * TextProcessing.java
+ * mhowse 2016
+ */
+
+public class TextProcessing{
+    
+    
+    public static void main(String [] args){
+        dateFormat();
+    }
+     
+    public static void dateFormat(){
+        
+    }
+}
